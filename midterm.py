cf8125a226b640416a047cec2b8073d7107d223e
jfoppes <jffoppes@gmail.com>
2023-09-23 20:58:12 +0300

    outline/first commmit

---
+++ midterm.py
@@ -0,0 +1,30 @@
+#!usr/bin/env python3
+#Jacob Foppes Midterm- Campsite Reservtions 2.0
+
+
+''' User will authenticate with a username and password or they will create one '''
+
+def welcome(): # User greeted with login or create new account option 
+    pass
+
+def login(): #exisiting useres login 
+    pass
+
+def createUsr(): # New users create accounts
+    pass
+
+while True:
+    break
+
+'''User will be able to view available sites and choose one to reserve, or cancel a reservation '''
+def view(): # 
+    pass
+
+def reserve(): # reserve will be able to input the day they want to reserve and it will be stored in a dictionary in a file local to the program 
+    pass
+
+def cancel():
+    pass
+
+while True: # while look for reservation system
+    break
